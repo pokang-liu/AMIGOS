de22d654f5c28add2cc525d3b2f37adc12e064e8
porkbaby <b03901062@ntu.edu.tw>
2017-10-23 23:41:30 +0800

    Add files via upload

--- fisher.py
+++ fisher.py
@@ -1,6 +1,7 @@
-def fisher(features,label):
+import numpy as np
+def fisher(features,labels):
 	Fisher=[]
-	label=np.array(label)
+	labels=np.array(labels)
 	labels0 = np.where(labels>0) 
 	labels1 = np.where(labels<1) 
 	labels0=np.array(labels0)
@@ -17,12 +18,12 @@ def fisher(features,label):
 
 	Fisher=(abs(mean_features0-mean_features1))/std_sum
 	Fisher=np.array(Fisher)
-	print(Fisher.shape)
+	print('Fisher.shape',Fisher.shape)
 	
 
 	#sort the fisher from small to large
 
-	feature_idx=np.arange(data.shape[1])
+	feature_idx=np.arange(features.shape[1])
 	Fisher_sorted = np.array(Fisher).argsort()
 	#Fisher_sorted[0] has the smallest score
 	#####################################################
@@ -31,10 +32,19 @@ def fisher(features,label):
 	return sorted_feature_idx
 	#####################################################
 
-def feature_selection(h)
+def feature_selection(h,features,sorted_feature_idx):
 	#only select h features
-	h_features=features[sorted_feature_idx[:h]]
+	print('sorted_feature_idx[:h]',sorted_feature_idx[:h])
+	print('sorted_feature_idx',sorted_feature_idx.size)
+	h_features=np.zeros((features.shape[0],h))
+	for i in range(features.shape[0]):
+		for j in range(h):	
+			h_features[i][j]=features[i][sorted_feature_idx[j]]
 	return h_features
 	
-	
+#########add these at line83######
+#sorted_v_feature_idx=fisher(train_data,train_a_labels)
+#train_v_data=feature_selection(150,train_data,sorted_v_feature_idx)	
 
+#sorted_a_feature_idx=fisher(train_data,train_a_labels)
+#train_a_data=feature_selection(150,train_data,sorted_v_feature_idx)
