efcdb9d728ea4a7c4a6ba95cad99e882cd0048cc
dtk0528 <tungkuan0528@gmail.com>
2017-11-06 12:24:28 +0800

    :sparkles: Switch to Gaussian Naïve Bayes. Use median for labels splitting

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from argparse import ArgumentParser
 import os
 import pickle
 import numpy as np
-from sklearn.svm import SVC
+# from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, f1_score
 
 from ALL_preprocess import MISSING_DATA, SUBJECT_NUM, VIDEO_NUM
@@ -27,14 +28,18 @@ if not os.path.exists(MODEL_DIR):
 def main():
     ''' Main function '''
     parser = ArgumentParser(description='Affective Computing with AMIGOS Dataset')
-    parser.add_argument('--feature', type=str, choices=['eeg', 'ecg', 'gsr', 'all'], default='all', help='choose type of modality')
+    parser.add_argument('--feature', type=str,
+                        choices=['eeg', 'ecg', 'gsr', 'all'], default='all', help='choose type of modality')
     args = parser.parse_args()
 
     with open(os.path.join('data', 'features.p'), 'rb') as pickle_file:
         amigos_data = pickle.load(pickle_file)
 
-    a_clf = SVC(C=0.25, kernel='linear')
-    v_clf = SVC(C=0.25, kernel='linear')
+    # a_clf = SVC(C=0.25, kernel='linear')
+    # v_clf = SVC(C=0.25, kernel='linear')
+
+    a_clf = GaussianNB()
+    v_clf = GaussianNB()
 
     train_a_accuracy_history = []
     train_v_accuracy_history = []
@@ -50,6 +55,7 @@ def main():
         train_data = np.array([])
         val_data = np.array([])
 
+        # get features for cross validation
         for s_key, data_dict in amigos_data.items():
             if args.feature == 'all':
                 tmp_array = np.array([])
@@ -60,7 +66,8 @@ def main():
                 if s_key.split('_')[0] == str(i + 1):
                     val_data = np.vstack((val_data, tmp_array)) if val_data.size else tmp_array
                 else:
-                    train_data = np.vstack((train_data, tmp_array)) if train_data.size else tmp_array
+                    train_data = np.vstack((train_data, tmp_array)
+                                           ) if train_data.size else tmp_array
             else:
                 tmp_array = np.array([])
                 for _, item in data_dict[args.feature].items():
@@ -69,15 +76,20 @@ def main():
                 if s_key.split('_')[0] == str(i + 1):
                     val_data = np.vstack((val_data, tmp_array)) if val_data.size else tmp_array
                 else:
-                    train_data = np.vstack((train_data, tmp_array)) if train_data.size else tmp_array
+                    train_data = np.vstack((train_data, tmp_array)
+                                           ) if train_data.size else tmp_array
 
+        # map features to [-1, 1]
         train_data_max = np.max(train_data, axis=0)
         train_data_min = np.min(train_data, axis=0)
         train_data = (train_data - train_data_min) / (train_data_max - train_data_min)
+        train_data = train_data * 2 - 1
         val_data_max = np.max(val_data, axis=0)
         val_data_min = np.min(val_data, axis=0)
         val_data = (val_data - val_data_min) / (val_data_max - val_data_min)
+        val_data = val_data * 2 - 1
 
+        # get labels for cross validation
         train_a_labels = []
         train_v_labels = []
         val_a_labels = []
@@ -89,13 +101,25 @@ def main():
                 if idx in MISSING_DATA_IDX:
                     continue
                 if idx in val_idx:
-                    val_a_labels.append(int(float(line.split(',')[0]) / 5))
-                    val_v_labels.append(int(float(line.split(',')[1]) / 5))
+                    val_a_labels.append(float(line.split(',')[0]))
+                    val_v_labels.append(float(line.split(',')[1]))
                 else:
-                    train_a_labels.append(int(float(line.split(',')[0]) / 5))
-                    train_v_labels.append(int(float(line.split(',')[1]) / 5))
+                    train_a_labels.append(float(line.split(',')[0]))
+                    train_v_labels.append(float(line.split(',')[1]))
                 if idx == SUBJECT_NUM * VIDEO_NUM - 1:
                     break
+
+        # split labels to 0 and 1 according to their median
+        train_a_labels_median = np.median(train_a_labels)
+        train_v_labels_median = np.median(train_v_labels)
+        val_a_labels_median = np.median(val_a_labels)
+        val_v_labels_median = np.median(val_v_labels)
+        for idx, label in enumerate(train_a_labels):
+            train_a_labels[idx] = 1 if label > train_a_labels_median else 0
+            train_v_labels[idx] = 1 if label > train_v_labels_median else 0
+        for idx, label in enumerate(val_a_labels):
+            val_a_labels[idx] = 1 if label > val_a_labels_median else 0
+            val_v_labels[idx] = 1 if label > val_v_labels_median else 0
 
         print('Training Arousal Model')
         a_clf.fit(train_data, train_a_labels)
