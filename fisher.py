cd2fab028b265859c91bf6af737f26026a77e569
frank30515 <frank30515@gmail.com>
2017-11-11 09:28:10 +0800

    calculate standard deviation for feature0 1 in each column

--- fisher.py
+++ fisher.py
@@ -2,33 +2,31 @@ import numpy as np
 
 
 def fisher(features, labels):
-    Fisher = []
     labels = np.array(labels)
-    labels0 = np.where(labels > 0)
-    labels1 = np.where(labels < 1)
-    labels0 = np.array(labels0)
-    features0 = np.delete(features, labels0, axis=0)
-    features1 = np.delete(features, labels1, axis=0)
+    labels0 = np.where(labels < 1)
+    labels1 = np.where(labels > 0)
+    labels0 = np.array(labels0).flatten()
+    labels1 = np.array(labels1).flatten()    
+    features0 = np.delete(features, labels1, axis=0)
+    features1 = np.delete(features, labels0, axis=0)
 
     mean_features0 = np.mean(features0, axis=0)
     mean_features1 = np.mean(features1, axis=0)
-
-    std_features0 = np.std(features0)
-    std_features1 = np.std(features1)
-    std_sum = (std_features1) * (std_features1) + std_features0 * std_features0
+    
+    std_features0 = np.std(features0, axis=0)
+    std_features1 = np.std(features1, axis=0)
+    std_sum = std_features1**2 + std_features0**2
 
     Fisher = (abs(mean_features0 - mean_features1)) / std_sum
     Fisher = np.array(Fisher)
     print('Fisher.shape', Fisher.shape)
 
     # sort the fisher from small to large
-
-    feature_idx = np.arange(features.shape[1])
-    Fisher_sorted = np.array(Fisher).argsort()
+    Fisher_sorted = np.argsort(Fisher)
     # Fisher_sorted[0] has the smallest score
     #####################################################
     # now Fisher_sorted[::-1]]'s head is the index with the largest score!!
-    sorted_feature_idx = feature_idx[Fisher_sorted[::-1]]
+    sorted_feature_idx = Fisher_sorted[::-1]
     return sorted_feature_idx
     #####################################################
 
